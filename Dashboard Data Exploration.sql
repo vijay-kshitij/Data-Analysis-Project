1875f625cf86f770e7b58a64a25af32bf6f32637
Kshitij Vijayvergiya <51355853+vijay-kshitij@users.noreply.github.com>
2024-10-22 20:07:22 +0530

    Update Dashboard Data Exploration.sql

--- Dashboard Data Exploration.sql
+++ Dashboard Data Exploration.sql
@@ -1,5 +1,5 @@
 -- Dashboard 1
-	-- Total Order, Total Sales, Total Items, Avg Order Value, Sales by Category, Top Selling Items, Orders by Hour, Sales by Hour, Orders by Address, Orders by Delivery
+	-- Total Order, Total Sales, Total Items, Avg Order Value, Sales by Category, Top Selling Items, Orders by Hour, Sales by Hour, Orders by Address
 
 	select
 		o.order_id,
@@ -24,7 +24,7 @@
 
 
 -- Dashboard 2
-	-- Total Quantity by Ingredients, Total Cost of Ingredients, Calculated Cost of Pizza, Percentage Stock Remaining by Ingredient
+	-- Total Quantity by Ingredients, Total Cost of Ingredients, Calculated Cost of Pizza
 
 	select
 		s1.item_name,
@@ -69,83 +69,9 @@
 		ing.ing_price
 ) s1
 
--- Creating view to continue querying Dashboard 2
-
-
-create view [stock1] as 
-
-	select
-		s1.item_name,
-		s1.ing_id,
-		s1.ing_name,
-		s1.ing_weight,
-		s1.ing_price,
-		s1.order_quantity,
-		s1.recipe_quantity,
-		s1.order_quantity * s1.recipe_quantity as ordered_weight,
-		s1.ing_price / s1.ing_weight as unit_cost,
-		(s1.order_quantity * s1.recipe_quantity) * (s1.ing_price / s1.ing_weight) as ingredient_cost
-	from 
-(
-	select
-		o.item_id,
-		i.sku,
-		i.item_name,
-		r.ing_id,
-		ing.ing_name,
-		r.quantity as recipe_quantity,
-		sum(o.quantity) as order_quantity,
-		ing.ing_weight,
-		cast(ing.ing_price as float) as ing_price
-	from orders o
-		left join item i
-			on o.item_id = i.item_id
-
-		left join recipe r
-			on i.sku = r.recipe_id
-
-		left join ingredient ing
-			on ing.ing_id = r.ing_id
-	group by 
-		o.item_id, 
-		i.sku, 
-		i.item_name,
-		r.ing_id,
-		r.quantity,
-		ing.ing_name,
-		ing.ing_weight,
-		ing.ing_price
-) s1;
-
-	select
-		*
-	from stock1
-
--- Proceeding with queries for Dashboard 2
-
-	select
-		s2.ing_name,
-		s2.ordered_weight,
-		cast(ing.ing_weight as bigint) *  cast(inv.quantity as bigint) as total_inv_weight,
-		(cast(ing.ing_weight as bigint) *  cast(inv.quantity as bigint)) - s2.ordered_weight as remaining_weight
-	from (
-		select 
-			ing_id,
-			ing_name,
-			sum(cast(ordered_weight as bigint)) as ordered_weight
-		from stock1
-		group by
-			ing_name,
-			ing_id) s2
-
-			left join inventory inv
-				on inv.item_id = s2.ing_id
-
-			left join ingredient ing
-				on ing.ing_id = s2.ing_id
 
 -- Dashboard 3
-	-- Total Staff Cost, Total Hours Worked, Staff Details Summary
+	-- Total Staff Cost, Total Hours Worked
 	select
 		r.date,
 		s.first_name,
